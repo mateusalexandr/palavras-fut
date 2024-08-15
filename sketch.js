63aa2ab8baf7187cd3d8dac622ffbd7c8ec4e114
mateus du rocki <145364645+mateusalexandr@users.noreply.github.com>
2024-08-15 10:37:29 -0300

    adicionando os arquivos

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["futi", "futi", "futibol"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("rgb(255,0,0)");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+  
+  inicializaCores();
+
+  let parcial = palavraParcial(0, width);
+    
+  text(parcial, 200, 200);
+  
+}
+
+ // função para site ...
